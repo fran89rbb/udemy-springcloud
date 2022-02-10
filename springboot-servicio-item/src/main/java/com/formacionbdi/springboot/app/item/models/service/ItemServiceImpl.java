04ee690476b2f3d6e842989d113e8572cf4d3028
Francisco <fran89rbb@hotmail.com>
2022-02-10 22:30:00 +0100

    Configuración básica item

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package com.formacionbdi.springboot.app.item.models.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+
+@Service
+public class ItemServiceImpl implements IItemService {
+
+	@Override
+	public List<Item> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
