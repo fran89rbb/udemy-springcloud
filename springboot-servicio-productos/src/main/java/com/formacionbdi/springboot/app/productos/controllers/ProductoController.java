f845bd2b9e4a5eb0accd3ad796b2daf2aec3b98f
Francisco <fran89rbb@hotmail.com>
2022-02-16 20:33:23 +0100

    Escalar microservcios con puerto dinámico

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,8 +1,10 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +18,21 @@ public class ProductoController {
 	@Autowired
 	private IProductoService productoService;
 	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
-		return productoService.findAll();
+		return productoService.findAll().stream().map(p -> {
+			p.setPort(port);
+			return p;
+		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id){
+		Producto producto = productoService.findById(id);
+		producto.setPort(port);
 		return productoService.findById(id);
 	}
 
