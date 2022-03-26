9ce8ce4b1c28e1d539cc1c7b4dfc6c91a827093c
Francisco <fran89rbb@hotmail.com>
2022-03-26 13:58:44 +0100

    Timeout

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,14 +1,14 @@
 package com.formacionbdi.springboot.app.item;
 
+import java.time.Duration;
+
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import ch.qos.logback.core.util.Duration;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
@@ -29,7 +29,7 @@ public class AppConfig {
 					.failureRateThreshold(150)
 					.waitDurationInOpenState(java.time.Duration.ofSeconds(10L))
 					.build())
-					.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+					.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2L)).build())
 					.build();
 		});
 	}
