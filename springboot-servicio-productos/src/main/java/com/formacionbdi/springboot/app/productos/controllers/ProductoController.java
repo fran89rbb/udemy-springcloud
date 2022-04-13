7a78ca844b15e2d307b732d4a84ea83ccc132499
Francisco <fran89rbb@hotmail.com>
2022-04-13 20:55:25 +0200

    Spring Cloud Gateway con Resilience4J

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,23 +18,28 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 public class ProductoController {
 	
 	@Autowired
-	private IProductoService productoService;
+	private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port;
 	
+	@Autowired
+	private IProductoService productoService;
+	
 	@GetMapping("/listar")
 	public List<Producto> listar(){
-		return productoService.findAll().stream().map(p -> {
-			p.setPort(port);
-			return p;
+		return productoService.findAll().stream().map(producto ->{
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(port);
+			return producto;
 		}).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) throws InterruptedException{
+	public Producto detalle(@PathVariable Long id) throws InterruptedException {
+		
 		if(id.equals(10L)) {
-			throw new IllegalStateException("Poducto no encontrado");
+			throw new IllegalStateException("Producto no encontrado!");
 		}
 		
 		if(id.equals(7L)) {
@@ -41,8 +47,9 @@ public class ProductoController {
 		}
 		
 		Producto producto = productoService.findById(id);
-		producto.setPort(port);
-		
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		//producto.setPort(port);
+				
 		return producto;
 	}
 
